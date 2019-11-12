e1c800cec61920ceac115c7fd017714bcd420b24
Aden <aden.lofthouse@gmail.com>
2019-11-13 10:25:05 +1100

    Final game version

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
 # Classic Arcade Game Clone Project
 
-## Table of Contents
-
-- [Instructions](#instructions)
-- [Contributing](#contributing)
-
 ## Instructions
 
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
-
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
-
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+The goal of this game is to reach the water! you start on the green grass at the bottom. Using the movement keys to walk along the stone tiles whilst avoiding the bugs.
+If you are hit by the bugs or if you successfully reach the water, you will be returned to the start!
--- js/app.js
+++ js/app.js
@@ -1,46 +1,91 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+class Enemy {
+  constructor(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = "images/enemy-bug.png";
+  }
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+    // Multiplies the speed by the dt parameter on the x axis
+    this.x += this.speed * dt;
+    //when enemies reach the end, they go back to the start with a different speed
+    if (this.x > 510) {
+      this.x = -50;
+      this.speed = 100 + Math.floor(Math.random() * 222);
+    }
+    // Checks for collisions between the player and the enemies
+    if (
+      player.x < this.x + 80 &&
+      player.x + 80 > this.x &&
+      player.y < this.y + 60 &&
+      60 + player.y > this.y
+    ) {
+      player.x = 202;
+      player.y = 405;
+    }
+  }
+  // Renders the enemy into the game
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = "images/char-boy.png";
+  }
+  update(dt) {}
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+  handleInput(kpress) {
+    if (kpress == "left" && this.x > 0) {
+      this.x -= 102;
+    }
+    if (kpress == "right" && this.x < 405) {
+      this.x += 102;
+    }
+    if (kpress == "up" && this.y > 0) {
+      this.y -= 83;
+    }
+    if (kpress == "down" && this.y < 405) {
+      this.y += 83;
+    }
+    if (this.y < 0) {
+      setTimeout(() => {
+        this.x = 202;
+        this.y = 405;
+      }, 800);
+    }
+  }
+}
 
+// All enemies are placed in an array
+var allEnemies = [];
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Location of the 3 enemies on the y axis located on the stone road
+var enemyLocation = [60, 150, 230];
 
+enemyLocation.forEach(function(Y) {
+  enemy = new Enemy(0, Y, 200);
+  allEnemies.push(enemy);
+});
 
+// The starting location of the player
+var player = new Player(200, 405);
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+// Player.handleInput() method.
+document.addEventListener("keyup", function(e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 });
